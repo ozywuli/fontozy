903831d7ba3a1bf1cd56b6c0e63d8610eb1804f2
Ozy Wu-Li <ozy.wu.li@gmail.com>
2014-04-17 17:20:25 -0700

    Overhauled the presentation of fonts; also decided to be more experimental

---
+++ javascripts/script.js
@@ -0,0 +1,9 @@
+// Fit Text
+
+$(function() {
+  $( '.farmers-market-text' ).fitText(2);
+  $( '.crimson' ).fitText(1);
+  $( '.theory-of-typography' ).fitText(1.5);
+  $( '#tot' ).find( 'p' ).fitText(3);
+
+});
